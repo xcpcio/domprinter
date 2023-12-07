ec0d66599561885674d16172de5d78e543882d67
Dup4 <lyuzhi.pan@gmail.com>
2023-12-08 01:07:33 +0800

    fix: sed
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- cmd/handle_print_cmd/exec.sh
+++ cmd/handle_print_cmd/exec.sh
@@ -17,7 +17,7 @@ if [[ -n "${SUBMIT_FILE_LIMIT}" ]]; then
     fi
 fi
 
-SOURCE_CODE="$(cat -n "${FILE}" | sed 's/\\n/\\\\n/g' | sed sed 's/\\r/\\\\r/g')"
+SOURCE_CODE="$(cat -n "${FILE}" | sed 's/\\n/\\\\n/g' | sed 's/\\r/\\\\r/g')"
 
 if [[ "$(uname -a | grep -c "MacBookPro")" -ge 1 ]]; then
     SUBMIT_TIME="2023-03-16T11:30:49.799+08:00"
